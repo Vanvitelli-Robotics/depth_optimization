cb21dfe2c3618b938b34edd818d000ed0b79a501
Marco19987 <marco1998.mds@gmail.com>
2023-10-12 13:18:53 +0200

    minor fix

--- setup_environment.sh
+++ setup_environment.sh
@@ -3,11 +3,13 @@
 # Import conda environment from YAML
 conda env create -f environment.yaml
 
+# little pause
+sleep 1
 # Activate the conda environment "py_depth_opt"
 conda activate py_depth_opt
 
 # Python version update for ROS2 compatibility 
-conda update python
+conda install python=3.10
 
 # Export python3.8 modules - This should be integrated in the bashrc
 export PYTHONPATH="$CONDA_PREFIX/lib/python3.8/site-packages:$PYTHONPATH"
